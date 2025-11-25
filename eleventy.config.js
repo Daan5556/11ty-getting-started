6c8306fdea8ca519595cdca17f009fb5b1062d2c
Daan <egdaan@gmail.com>
2025-11-25 16:00:52 +0100

    Added css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,11 @@
 // @ts-check
 
+import CleanCSS from "clean-css";
+
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src/");
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 }
